f16449ff4b82ffc3a163917551a048aa0324f223
squarematr1x <sasu.pirttipera@protonmail.com>
2020-08-30 21:43:10 +0300

    Added and implemented function 'ApplyBinomialTheorem'.

--- YAASC/src/PowerOfSum.cpp
+++ YAASC/src/PowerOfSum.cpp
@@ -19,38 +19,16 @@ void PowerOfSum(std::unique_ptr<Expr>& expr)
 	if (!CanApplyPowerOfSum(expr))
 		return;
 
-	std::unique_ptr<Expr>& add_node = expr->Left();
-	std::unique_ptr<Expr>& exponent = expr->Right();
-	std::unique_ptr<Expr> new_add_node = std::make_unique<Mul>();
-
 	// (a+b)^n
-	if (!add_node->IsGeneric())
-	{
-		int multiplications = stoi(exponent->Name());
-
-		// (a+b)^2
-		if (multiplications == 2)
-		{
-			std::unique_ptr<Expr> copy;
-			tree_util::DeepCopy(copy, add_node);
-			expr = std::make_unique<Mul>(std::move(add_node), std::move(copy));
-		}
-		// (a+b)^n, where n > 2
-		else if (multiplications > 2)
-		{
-			for (int i = 0; i < multiplications; i++)
-			{
-				std::unique_ptr<Expr> copy;
-				tree_util::DeepCopy(copy, add_node);
-				new_add_node->AddChild(std::move(copy));
-			}
-
-			expr = std::move(new_add_node);
-		}
-	}
+	if (!expr->Left()->IsGeneric())
+		ApplyBinomialTheorem(expr);
 	// (a+b+...m)^n
 	else
 	{
+		std::unique_ptr<Expr>& add_node = expr->Left();
+		std::unique_ptr<Expr>& exponent = expr->Right();
+		std::unique_ptr<Expr> new_add_node = std::make_unique<Mul>();
+
 		int multiplications = stoi(exponent->Name());
 
 		// (a+b+c)^2, in other cases the output string is inconveniently large
@@ -68,6 +46,44 @@ void PowerOfSum(std::unique_ptr<Expr>& expr)
 	}
 }
 
+// (a+b)^n
+void ApplyBinomialTheorem(std::unique_ptr<Expr>& expr)
+{
+	std::unique_ptr<Expr>& exponent = expr->Right();
+	std::unique_ptr<Expr> add_node = std::move(expr->Left());
+	std::unique_ptr<Expr> new_add_node = std::make_unique<Add>();
+
+	int n = stoi(exponent->Name());
+	int numerator = calc::Factorial(n);
+	int coefficient = 1;
+
+	for (int i = 0; i <= n; i++)
+	{
+		int denominator = calc::Factorial(i) * calc::Factorial(n - i);
+		coefficient = numerator / denominator;
+
+		std::unique_ptr<Expr> mul_node = std::make_unique<Mul>();
+
+		std::unique_ptr<Expr> a;
+		std::unique_ptr<Expr> b;
+		tree_util::DeepCopy(a, add_node->Left());
+		tree_util::DeepCopy(b, add_node->Right());
+
+		mul_node->AddChild(std::move(std::make_unique<Integer>(coefficient)));
+
+		if (n - i > 0)
+			mul_node->AddChild(std::move(std::make_unique<Pow>(std::move(a), std::make_unique<Integer>(n - i))));
+
+		if (i > 0)
+			mul_node->AddChild(std::move(std::make_unique<Pow>(std::move(b), std::make_unique<Integer>(i))));
+
+		new_add_node->AddChild(std::move(mul_node));
+	}
+
+	coefficient = 0;
+	expr = std::move(new_add_node);
+}
+
 bool CanApplyPowerOfSum(const std::unique_ptr<Expr>& expr)
 {
 	// Expression is (a+b+...m)^n and m is integer
@@ -77,4 +93,4 @@ bool CanApplyPowerOfSum(const std::unique_ptr<Expr>& expr)
 	return false;
 }
 
-}
+} // namespace algebra
--- YAASC/src/PowerOfSum.h
+++ YAASC/src/PowerOfSum.h
@@ -2,11 +2,13 @@
 
 #include "Expr.h"
 #include "TreeUtil.h"
+#include "Calculator.h"
 
 namespace algebra {
 
 void PowerOfSum(std::unique_ptr<Expr>& expr);
+void ApplyBinomialTheorem(std::unique_ptr<Expr>& expr);
 
 bool CanApplyPowerOfSum(const std::unique_ptr<Expr>& expr);
 
-}
+} // namespace algebra

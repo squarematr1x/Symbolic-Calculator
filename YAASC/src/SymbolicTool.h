0b3465aa3c96aaa0fe8aa3da292ad2d8b368fa4b
squarematr1x <sasu.pirttipera@protonmail.com>
2020-09-09 16:33:00 +0300

    Moved functions from SymbolicTool.

--- YAASC/src/SymbolicTool.h
+++ YAASC/src/SymbolicTool.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Addition.h"
 #include "PowerTransformation.h"
 #include "PowerOfSum.h"
 #include "Expand.h"
@@ -9,13 +10,7 @@
 namespace yaasc {
 
 void Simplify(std::unique_ptr<Expr>& expr);
-
-void AddVariables(std::unique_ptr<Expr>& root);
-void AddBinNode(std::unique_ptr<Expr>& root, std::unique_ptr<Expr>& left, std::unique_ptr<Expr>& right);
-void AddGenNode(std::unique_ptr<Expr>& root);
-
 void SimplifyExponents(std::unique_ptr<Expr>& root, bool final_modification);
-void ApplyExponentRules(std::unique_ptr<Expr>& root);
 
 void RemoveMulOne(std::unique_ptr<Expr>& root);
 void RemoveAdditiveZeros(std::unique_ptr<Expr>& root);
@@ -32,15 +27,9 @@ void CanonizeGenNode(std::unique_ptr<Expr>& root);
 void ReduceGenNodeToBinNode(std::unique_ptr<Expr>& root);
 
 bool Simplified(const std::unique_ptr<Expr>& expr_a, const std::unique_ptr<Expr>& expr_b);
-
-bool CanAddGen(const std::unique_ptr<Expr>& expr_a, const std::unique_ptr<Expr>& expr_b); 
-
-bool SameGenericVariables(const std::unique_ptr<Expr>& expr_a, const std::unique_ptr<Expr>& expr_b);
 bool SameExpressionTypes(const std::unique_ptr<Expr>& expr_a, const std::unique_ptr<Expr>& expr_b);
 
 bool RaisedToZero(const std::unique_ptr<Expr>& expr);
 bool RaisedToOne(const std::unique_ptr<Expr>& expr);
-bool IsMulByNumber(const std::unique_ptr<Expr>& expr);
-bool IsGenMulByNumber(const std::unique_ptr<Expr>& expr);
 
 } // namespace yaasc
